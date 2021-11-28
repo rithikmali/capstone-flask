313b52b3847221c03ec69ed8cabe9272c04f5822
rithikmali <51767737+rithikmali@users.noreply.github.com>
2021-11-28 13:26:14 +0530

    aa

--- application/app.py
+++ application/app.py
@@ -7,6 +7,7 @@ from datetime import timedelta
 #connect to database
 # db_url = os.environ["MONGODB_URI"]
 db_url = "mongodb+srv://rithik:capstoneproject@capstone1.86sce.mongodb.net/capstone1?retryWrites=true&w=majority"
+
 client = pymongo.MongoClient(db_url)
 db = client.capstone
 
